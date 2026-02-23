2bc3c8c39f6b70406cba17298b4e76fd50ba06c3
roshani-daulkar <roshanidaulkar1@gmail.com>
2026-02-24 00:30:34 +0530

    created post method using mongo db connection

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -3,7 +3,14 @@ const ownersRouter = express.Router();
 const ownerModel = require("../models/owners_models");
 
 ownersRouter.get("/" , (req , res) => {
-    res.send("Hey");
+    res.send("Hey its working");
 });
+
+ownersRouter.post("/create" , async function (req , res) {
+    let owner = await ownerModel.find();
+    if (owner.length > 0) return res.send(503).send("You are not authorized to create new owner");
+    res.send("we can create owner")
+    
+})
 
 module.exports = ownersRouter;
